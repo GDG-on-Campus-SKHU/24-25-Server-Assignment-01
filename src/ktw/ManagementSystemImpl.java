478d47738afd42cfdae6e436023349db9fb77a9d
twootwoo <mbnman12@gmail.com>
2024-09-29 03:11:36 +0900

    Chore : ktw.ManagementSystemImpl.java 리팩토링
    0. 불필요한 변수 제거 및 return문으로 병합
    1. 불필요한 else문을 삭제하여 early return 스타일로 코드 수정
    2. 조건문 뒤에 위치하는 소괄호를 한 칸씩 띄워줌
    3. 메인 로직과 return문을 한 줄씩 띄워줌
    4. 2,3번 작업으로 코드의 가독성 향상 및 코드 작성 관례를 따르도록 수정함
    5. printMembers() 메소드의 로직 오류 수정

--- src/ktw/ManagementSystemImpl.java
+++ src/ktw/ManagementSystemImpl.java
@@ -7,105 +7,90 @@ import java.util.stream.Collectors;
 import jun.Member;
 import jun.ManagementSystem;
 
-import jun.Member;
-;
 public class ManagementSystemImpl implements ManagementSystem {
 
-
-    HashSet<Member> members = new HashSet<Member>();
+    // Set타입을 사용한 것을 직관적으로 알 수 있도록 변수명을 membersSet 으로 지정함
+    // 코드의 복잡도가 높지 않아 굳이 Key,Value 형태의 Map을 사용할 필요가 없다고 판단했으며
+    // 자료형 하나를 사용해 직관성이 더 높다고 생각되는 Set을 사용했습니다
+    HashSet<Member> membersSet = new HashSet<Member>();
 
     @Override
     public boolean addMember(String name, int age, int id) {
         // findById를 날렸을때 값이 없으면 멤버를 추가함
-        if(findById(id) == null) {
-            members.add(new Member(name, age, id));
+        if (findById(id) == null) {
+            membersSet.add(new Member(name, age, id));
+
             return true;
         }
         // findByID 날렸을때 값이 있으면 추가 불가능
-        else {
-            return false;
-        }
+        return false;
     }
 
     @Override
     public boolean removeMember(int id) {
         // findById를 날렸는데 값이 없으면 멤버 삭제 불가
-        if(findById(id) == null) {
+        if (findById(id) == null) {
             return false;
         }
         // findById를 날렸는데 값이 있으면 그 멤버 객체를 set에서 삭제
-        else {
-            members.remove(findById(id));
-            return true;
-        }
+        membersSet.remove(findById(id));
 
+        return true;
     }
 
     @Override
     public boolean updateMember(int id, String name) {
-        // 1. id로 찾아서 없는 멤버면 수정불가
-        if(findById(id) == null) {
+        // id로 찾아서 없는 멤버면 수정불가
+        if (findById(id) == null) {
             return false;
         }
-        // 2. id로 찾아서 있는 멤버면 get()으로 그 객체의 필드변수 수정
-        else {
-            findById(id).setName(name);
-            return true;
-        }
+        // id로 찾아서 있는 멤버면 get()으로 그 객체의 필드변수 수정
+        findById(id).setName(name);
+
+        return true;
     }
 
     @Override
     public boolean updateMember(int id, int age) {
-        // 1. id로 찾아서 없는 멤버면 수정불가
-        if(findById(id) == null) {
+        // id로 찾아서 없는 멤버면 수정불가
+        if (findById(id) == null) {
             return false;
         }
-        // 2. id로 찾아서 있는 멤버면 get()으로 그 객체의 필드변수 수정
-        else {
-            findById(id).setAge(age);
-            return true;
-        }
+        // id로 찾아서 있는 멤버면 get()으로 그 객체의 필드변수 수정
+        findById(id).setAge(age);
+
+        return true;
     }
 
     @Override
     public boolean updateMember(int id, String name, int age) {
-        // 1. id로 찾아서 없는 멤버면 수정불가
-        if(findById(id) == null) {
+        // id로 찾아서 없는 멤버면 수정불가
+        if (findById(id) == null) {
             return false;
         }
-        // 2. id로 찾아서 있는 멤버면 get()으로 그 객체의 필드변수 수정
-        else {
-            findById(id).setName(name);
-            findById(id).setAge(age);
-            return true;
-        }
+        // id로 찾아서 있는 멤버면 get()으로 그 객체의 필드변수 수정
+        findById(id).setName(name);
+        findById(id).setAge(age);
+
+        return true;
     }
 
     @Override
     public Member findById(int id) {
-        return members.stream()
+        return membersSet.stream()
                 .filter(member -> member.getId() == id)
                 .findFirst()
                 .orElse(null);
     }
-            /*
-        if(tempResult.isPresent()){
-            return tempResult.get();
-        } else {
-            return null;
-        }
-        -> orElse로 축약 가능
-         */
 
-   /*
+    /*
         구조 파악을 위해 하나씩 풀어 써본 코드
         1. 해쉬셋 -> 스트림 변환
-        Stream<Member> memberStream = members.stream();
+        Stream<Member> memberStream = membersSet.stream();
         2. 스트림의 filter메소드로 싹 돌면서 조건에 맞춰 필터링
         Stream<Member> filteredStream = memberStream.filter(member -> member.getId()==id);
         3. 스트림의 findFirst 메소드 이용, 첫 번째 요소가 있으면 그 요소 저장, 없으면 Optional.empty() 저장
         Optional<Member> tempResult = filteredStream.findFirst();
-
 
         4. tempResult에 값이 있으면 옵셔널 벗겨서 반환
         if(result.isPresent()) {
@@ -114,28 +99,31 @@ public class ManagementSystemImpl implements ManagementSystem {
         } else {
             return null;
         }
-   }
+        -> 4,5번 과정을 orElse(null)로 축약 가능
     */
 
     @Override
     public List<Member> findMembers(Predicate<Member> filter) {
-        return members.stream().filter(filter).collect(Collectors.toList());
-        // 자바 16부터는 stream 자체에 toList() 메소드가 도입되어 이렇게도 쓸 수 있다고 한다
-        // return members.stream().filter(filter).toList();
+        // 자바 16부터 도입된 toList() 메소드 사용, 백준에서는 자바 15까지 지원하므로 사용불가
+        return membersSet.stream().filter(filter).toList();
+        // 아래와 같은 방법으로도 리스트로 변환 가능
+        // 스트림의 모든 요소들을 리스트로 수집하는 Collector 객체를 생성한뒤 toList() 로 리스트로 변환
+        // return membersSet.stream().filter(filter).collect(Collectors.toList());
     }
 
     @Override
     public Optional<Member> findMember(Predicate<Member> filter) {
-        return members.stream().filter(filter).findFirst();
+        return membersSet.stream().filter(filter).findFirst();
     }
 
     @Override
     public void printMembers() {
-        members.forEach(member -> System.out.println("toString"));
+        // 리스트 형태의 자료구조를 싹 돌면서 각 요소에 대해 내가 원하는 작업을 해주는 forEach() 메소드 사용
+        membersSet.forEach(member -> System.out.println(member.toString()));
     }
 
     @Override
     public int getSize() {
-        return members.size();
+        return membersSet.size();
     }
 }
